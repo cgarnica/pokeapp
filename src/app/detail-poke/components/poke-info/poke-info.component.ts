8cca631d04f7cd436998b2c8a1696e3109acddf8
judavid <judavid.arias@gmail.com>
2019-01-19 13:52:05 -0500

    se agrega la lista que despliega todas las colecciones (primera parte)

--- src/app/detail-poke/components/poke-info/poke-info.component.ts
+++ src/app/detail-poke/components/poke-info/poke-info.component.ts
@@ -1,18 +1,70 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Observable } from 'rxjs';
+import { ICollection } from 'src/app/collections/models/collection';
+import { CollectionService } from 'src/app/collections/services/collections.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-poke-info',
   templateUrl: './poke-info.component.html',
-  styleUrls: ['./poke-info.component.css']
+  styleUrls: ['./poke-info.component.css'],
+  animations: [
+    trigger('popupAnimation', [
+      state('closed', style({
+        height: '0px',
+        display: 'none'
+      })),
+      state('opened', style({
+        height: '200px',
+      })),
+      transition('closed => opened', animate('90ms')),
+      transition('opened => closed', animate('90ms'))
+    ])
+  ]
 })
 export class PokeInfoComponent implements OnInit {
 
   @Input()
   id:string;
 
-  constructor() { }
+
+  listCollections: Observable<any[]>;
+  collectionSelected:ICollection;
+  state:string;
+  popupState:string;
+
+
+  constructor(private collectionService:CollectionService, private authFire: AngularFireAuth) {
+    this.listCollections = null;
+    this.collectionSelected = null;
+    this.state = 'open';
+    this.popupState = 'closed';
+   }
 
   ngOnInit() {
+    this.authFire.authState
+    .subscribe(
+      user => {          
+        this.listCollections =  this.collectionService.getListCollections().snapshotChanges().pipe(
+          map(changes => 
+            changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+          ));
+      }
+    ); 
+  }
+
+  addToCollection(event:any){
+
+    //this.collectionService.addBook(event.target.value, this.book);
+  }
+
+  onNavImageClick(){
+    if(this.popupState == 'closed')
+      this.popupState = 'opened'
+    else
+      this.popupState = 'closed'
   }
 
 }

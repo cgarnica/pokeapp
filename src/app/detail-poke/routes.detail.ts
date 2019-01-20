a56d3450edea98c219a9bfad93b86237fedeeee2
judavid <judavid.arias@gmail.com>
2019-01-19 21:56:29 -0500

    se arregla error de path repetido, no mostraba la lista de pokemons al ingresar

--- src/app/detail-poke/routes.detail.ts
+++ src/app/detail-poke/routes.detail.ts
@@ -2,10 +2,7 @@ import { Routes } from "@angular/router";
 import { PokeDetailComponent } from "./containers/poke-detail/poke-detail.component";
 
 export const routes: Routes = [
-    {
-        path: 'list',
-        component: PokeDetailComponent
-    },{
+   {
         path: 'detail-poke/:id',
         component: PokeDetailComponent
     },
